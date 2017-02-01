591460a3563d4a26f20ba8b03badaa9ab72ac24b
Ayush Saraf <ayush29f@uw.edu>
2017-01-31 17:42:00 -0800

    removed redundant files

--- assignment2/data_utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def load_data(filepath):
-    df = pd.read_csv(filepath)
-    X = df.iloc[:, 1:].values
-    y = df.iloc[:, :1].values.reshape(-1)
-
-    return (X, y)
